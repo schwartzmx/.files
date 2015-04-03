29e3d15ce8c7c2ba1cc5d5bc4bf09dbbff5f5885
Phil <schwartzmx@gmail.com>
2015-04-03 11:22:14 -0500

    update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,4 +1,5 @@
 Set-Location $HOME
+Set-Variable HOSTS "C:\Windows\System32\drivers\etc\hosts" -Force
 
 # Load posh-git example profile
 if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
@@ -16,10 +17,26 @@ Function Edit-Profile
     vim $profile
 }
 
-# For Editing your Vim settings
-Function Edit-Vimrc
+# Edit hosts file
+Function Edit-Hosts
 {
+  vim $HOSTS
+}
+
+# For Editing your Vim settings
+Function Edit-Vimrc{
     vim $home\_vimrc
+}
+
+# Find files with a specified string in them
+# String Search Directory > ss-dir Directory String
+Function ss-dir {
+	If (Test-Path -Path $args[0] -PathType Container) {
+		Get-ChildItem $args[0] -recurse | Select-String -pattern “$args[1]” | Group path | Select name
+	}
+	Else {
+		Write-Host "Path specified is not a directory. Usage: ss-dir Directory String"
+	}
 }
 
 # Quick access to Projects
@@ -34,6 +51,60 @@ Function rmrf {
     }
 }
 
+
+function New-Zip
+{
+    param([string]$zipfilename)
+    set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+    (dir $zipfilename).IsReadOnly = $false
+}
+# usage: new-zip c:\demo\myzip.zip
+
+function Add-Zip
+{
+    param([string]$zipfilename)
+
+    if(-not (test-path($zipfilename))) {
+        set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+        (dir $zipfilename).IsReadOnly = $false  
+    }
+                        
+    $shellApplication = new-object -com shell.application
+    $zipPackage = $shellApplication.NameSpace($zipfilename)
+                              
+    foreach($file in $input) { 
+        $zipPackage.CopyHere($file.FullName)
+        Start-sleep -milliseconds 500
+    }
+}
+#usage: dir c:\demo\files\*.* -Recurse | add-Zip c:\demo\myzip.zip
+
+function Get-Zip
+{
+    param([string]$zipfilename)
+    if(test-path($zipfilename)) {
+        $shellApplication = new-object -com shell.application
+        $zipPackage = $shellApplication.NameSpace($zipfilename)
+        $zipPackage.Items() | Select Path
+    }
+}
+
+#usage: Get-Zip c:\demo\myzip.zip
+
+function Extract-Zip
+{
+    param([string]$zipfilename, [string] $destination)
+
+    if(test-path($zipfilename)) { 
+        $shellApplication = new-object -com shell.application
+        $zipPackage = $shellApplication.NameSpace($zipfilename)
+        $destinationFolder = $shellApplication.NameSpace($destination)
+        $destinationFolder.CopyHere($zipPackage.Items())
+    }
+}
+
+
+New-Alias import Import-Module
 New-Alias cl clear
 New-Alias which get-command
 New-Alias open explorer
@@ -42,75 +113,77 @@ Set-Alias subl -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
 
 # Assumes GitBash is installed
 # $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"      
-Set-Alias antiword -Value "C:\Program Files (x86)\Git\bin\antiword.exe"
-Set-Alias basename -Value "C:\Program Files (x86)\Git\bin\basename.exe"
-Set-Alias bash -Value "C:\Program Files (x86)\Git\bin\bash.exe"
-Set-Alias bison -Value "C:\Program Files (x86)\Git\bin\bison.exe"
-Set-Alias bzip2 -Value "C:\Program Files (x86)\Git\bin\bzip2.exe"
-Set-Alias chmod -Value "C:\Program Files (x86)\Git\bin\chmod.exe"
-Set-Alias cmp -Value "C:\Program Files (x86)\Git\bin\cmp.exe"
-Set-Alias connect -Value "C:\Program Files (x86)\Git\bin\connect.exe"
-Set-Alias cut -Value "C:\Program Files (x86)\Git\bin\cut.exe"
-Set-Alias date -Value "C:\Program Files (x86)\Git\bin\date.exe"
-Set-Alias dirname -Value "C:\Program Files (x86)\Git\bin\dirname.exe"
-Set-Alias dos2unix -Value "C:\Program Files (x86)\Git\bin\dos2unix.exe"
-Set-Alias du -Value "C:\Program Files (x86)\Git\bin\du.exe"
-Set-Alias env -Value "C:\Program Files (x86)\Git\bin\env.exe"
-Set-Alias expr -Value "C:\Program Files (x86)\Git\bin\expr.exe"
-Set-Alias false -Value "C:\Program Files (x86)\Git\bin\false.exe"
-Set-Alias find -Value "C:\Program Files (x86)\Git\bin\find.exe"
-Set-Alias flex -Value "C:\Program Files (x86)\Git\bin\flex.exe"
-Set-Alias gawk -Value "C:\Program Files (x86)\Git\bin\gawk.exe"
-Set-Alias getcp -Value "C:\Program Files (x86)\Git\bin\getcp.exe"
-Set-Alias git -Value "C:\Program Files (x86)\Git\bin\git.exe"
-Set-Alias gpg -Value "C:\Program Files (x86)\Git\bin\gpg.exe"
-Set-Alias gpgkeys_curl -Value "C:\Program Files (x86)\Git\bin\gpgkeys_curl.exe"
-Set-Alias gpgkeys_finger -Value "C:\Program Files (x86)\Git\bin\gpgkeys_finger.exe"
-Set-Alias gpgkeys_hkp -Value "C:\Program Files (x86)\Git\bin\gpgkeys_hkp.exe"
-Set-Alias gpgkeys_ldap -Value "C:\Program Files (x86)\Git\bin\gpgkeys_ldap.exe"
-Set-Alias gpgsplit -Value "C:\Program Files (x86)\Git\bin\gpgsplit.exe"
-Set-Alias gpgv -Value "C:\Program Files (x86)\Git\bin\gpgv.exe"
-Set-Alias grep -Value "C:\Program Files (x86)\Git\bin\grep.exe"
-Set-Alias gunzip -Value "C:\Program Files (x86)\Git\bin\gunzip.exe"
-Set-Alias gzip -Value "C:\Program Files (x86)\Git\bin\gzip.exe"
-Set-Alias hd2u -Value "C:\Program Files (x86)\Git\bin\hd2u.exe"
-Set-Alias head -Value "C:\Program Files (x86)\Git\bin\head.exe"
-Set-Alias iconv -Value "C:\Program Files (x86)\Git\bin\iconv.exe"
-Set-Alias id -Value "C:\Program Files (x86)\Git\bin\id.exe"
-Set-Alias less -Value "C:\Program Files (x86)\Git\bin\less.exe"
-Set-Alias ln -Value "C:\Program Files (x86)\Git\bin\ln.exe"
-Set-Alias m4 -Value "C:\Program Files (x86)\Git\bin\m4.exe"
-Set-Alias md5sum -Value "C:\Program Files (x86)\Git\bin\md5sum.exe"
-Set-Alias mkdir -Value "C:\Program Files (x86)\Git\bin\mkdir.exe"
-Set-Alias msmtp -Value "C:\Program Files (x86)\Git\bin\msmtp.exe"
-Set-Alias openssl -Value "C:\Program Files (x86)\Git\bin\openssl.exe"
-Set-Alias patch -Value "C:\Program Files (x86)\Git\bin\patch.exe"
-Set-Alias pdfinfo -Value "C:\Program Files (x86)\Git\bin\pdfinfo.exe"
-Set-Alias pdftotext -Value "C:\Program Files (x86)\Git\bin\pdftotext.exe"
-Set-Alias perl -Value "C:\Program Files (x86)\Git\bin\perl.exe"
-Set-Alias rebase -Value "C:\Program Files (x86)\Git\bin\rebase.exe"
-Set-Alias rxvt -Value "C:\Program Files (x86)\Git\bin\rxvt.exe"
-Set-Alias scp -Value "C:\Program Files (x86)\Git\bin\scp.exe"
-Set-Alias sed -Value "C:\Program Files (x86)\Git\bin\sed.exe"
-Set-Alias sh -Value "C:\Program Files (x86)\Git\bin\sh.exe"
-Set-Alias split -Value "C:\Program Files (x86)\Git\bin\split.exe"
-Set-Alias ssh-add -Value "C:\Program Files (x86)\Git\bin\ssh-add.exe"
-Set-Alias ssh-agent -Value "C:\Program Files (x86)\Git\bin\ssh-agent.exe"
-Set-Alias ssh-keygen -Value "C:\Program Files (x86)\Git\bin\ssh-keygen.exe"
-Set-Alias ssh-keyscan -Value "C:\Program Files (x86)\Git\bin\ssh-keyscan.exe"
-Set-Alias ssh -Value "C:\Program Files (x86)\Git\bin\ssh.exe"
-Set-Alias tail -Value "C:\Program Files (x86)\Git\bin\tail.exe"
-Set-Alias tar -Value "C:\Program Files (x86)\Git\bin\tar.exe"
-Set-Alias tclsh -Value "C:\Program Files (x86)\Git\bin\tclsh.exe"
-Set-Alias tclsh85 -Value "C:\Program Files (x86)\Git\bin\tclsh85.exe"
-Set-Alias touch -Value "C:\Program Files (x86)\Git\bin\touch.exe"
-Set-Alias tr -Value "C:\Program Files (x86)\Git\bin\tr.exe"
-Set-Alias true -Value "C:\Program Files (x86)\Git\bin\true.exe"
-Set-Alias uname -Value "C:\Program Files (x86)\Git\bin\uname.exe"
-Set-Alias uniq -Value "C:\Program Files (x86)\Git\bin\uniq.exe"
-Set-Alias unix2dos -Value "C:\Program Files (x86)\Git\bin\unix2dos.exe"
-Set-Alias unzip -Value "C:\Program Files (x86)\Git\bin\unzip.exe"
-Set-Alias wc -Value "C:\Program Files (x86)\Git\bin\wc.exe"
-Set-Alias wish -Value "C:\Program Files (x86)\Git\bin\wish.exe"
-Set-Alias wish85 -Value "C:\Program Files (x86)\Git\bin\wish85.exe"
-Set-Alias xargs -Value "C:\Program Files (x86)\Git\bin\xargs.exe"
+If (Test-Path -Path "C:\Program Files (x86)\Git\bin\" -PathType Container) {
+  Set-Alias antiword -Value "C:\Program Files (x86)\Git\bin\antiword.exe"
+  Set-Alias basename -Value "C:\Program Files (x86)\Git\bin\basename.exe"
+  Set-Alias bash -Value "C:\Program Files (x86)\Git\bin\bash.exe"
+  Set-Alias bison -Value "C:\Program Files (x86)\Git\bin\bison.exe"
+  Set-Alias bzip2 -Value "C:\Program Files (x86)\Git\bin\bzip2.exe"
+  Set-Alias chmod -Value "C:\Program Files (x86)\Git\bin\chmod.exe"
+  Set-Alias cmp -Value "C:\Program Files (x86)\Git\bin\cmp.exe"
+  Set-Alias connect -Value "C:\Program Files (x86)\Git\bin\connect.exe"
+  Set-Alias cut -Value "C:\Program Files (x86)\Git\bin\cut.exe"
+  Set-Alias date -Value "C:\Program Files (x86)\Git\bin\date.exe"
+  Set-Alias dirname -Value "C:\Program Files (x86)\Git\bin\dirname.exe"
+  Set-Alias dos2unix -Value "C:\Program Files (x86)\Git\bin\dos2unix.exe"
+  Set-Alias du -Value "C:\Program Files (x86)\Git\bin\du.exe"
+  Set-Alias env -Value "C:\Program Files (x86)\Git\bin\env.exe"
+  Set-Alias expr -Value "C:\Program Files (x86)\Git\bin\expr.exe"
+  Set-Alias false -Value "C:\Program Files (x86)\Git\bin\false.exe"
+  Set-Alias find -Value "C:\Program Files (x86)\Git\bin\find.exe"
+  Set-Alias flex -Value "C:\Program Files (x86)\Git\bin\flex.exe"
+  Set-Alias gawk -Value "C:\Program Files (x86)\Git\bin\gawk.exe"
+  Set-Alias getcp -Value "C:\Program Files (x86)\Git\bin\getcp.exe"
+  Set-Alias git -Value "C:\Program Files (x86)\Git\bin\git.exe"
+  Set-Alias gpg -Value "C:\Program Files (x86)\Git\bin\gpg.exe"
+  Set-Alias gpgkeys_curl -Value "C:\Program Files (x86)\Git\bin\gpgkeys_curl.exe"
+  Set-Alias gpgkeys_finger -Value "C:\Program Files (x86)\Git\bin\gpgkeys_finger.exe"
+  Set-Alias gpgkeys_hkp -Value "C:\Program Files (x86)\Git\bin\gpgkeys_hkp.exe"
+  Set-Alias gpgkeys_ldap -Value "C:\Program Files (x86)\Git\bin\gpgkeys_ldap.exe"
+  Set-Alias gpgsplit -Value "C:\Program Files (x86)\Git\bin\gpgsplit.exe"
+  Set-Alias gpgv -Value "C:\Program Files (x86)\Git\bin\gpgv.exe"
+  Set-Alias grep -Value "C:\Program Files (x86)\Git\bin\grep.exe"
+  Set-Alias gunzip -Value "C:\Program Files (x86)\Git\bin\gunzip.exe"
+  Set-Alias gzip -Value "C:\Program Files (x86)\Git\bin\gzip.exe"
+  Set-Alias hd2u -Value "C:\Program Files (x86)\Git\bin\hd2u.exe"
+  Set-Alias head -Value "C:\Program Files (x86)\Git\bin\head.exe"
+  Set-Alias iconv -Value "C:\Program Files (x86)\Git\bin\iconv.exe"
+  Set-Alias id -Value "C:\Program Files (x86)\Git\bin\id.exe"
+  Set-Alias less -Value "C:\Program Files (x86)\Git\bin\less.exe"
+  Set-Alias ln -Value "C:\Program Files (x86)\Git\bin\ln.exe"
+  Set-Alias m4 -Value "C:\Program Files (x86)\Git\bin\m4.exe"
+  Set-Alias md5sum -Value "C:\Program Files (x86)\Git\bin\md5sum.exe"
+  Set-Alias mkdir -Value "C:\Program Files (x86)\Git\bin\mkdir.exe"
+  Set-Alias msmtp -Value "C:\Program Files (x86)\Git\bin\msmtp.exe"
+  Set-Alias openssl -Value "C:\Program Files (x86)\Git\bin\openssl.exe"
+  Set-Alias patch -Value "C:\Program Files (x86)\Git\bin\patch.exe"
+  Set-Alias pdfinfo -Value "C:\Program Files (x86)\Git\bin\pdfinfo.exe"
+  Set-Alias pdftotext -Value "C:\Program Files (x86)\Git\bin\pdftotext.exe"
+  Set-Alias perl -Value "C:\Program Files (x86)\Git\bin\perl.exe"
+  Set-Alias rebase -Value "C:\Program Files (x86)\Git\bin\rebase.exe"
+  Set-Alias rxvt -Value "C:\Program Files (x86)\Git\bin\rxvt.exe"
+  Set-Alias scp -Value "C:\Program Files (x86)\Git\bin\scp.exe"
+  Set-Alias sed -Value "C:\Program Files (x86)\Git\bin\sed.exe"
+  Set-Alias sh -Value "C:\Program Files (x86)\Git\bin\sh.exe"
+  Set-Alias split -Value "C:\Program Files (x86)\Git\bin\split.exe"
+  Set-Alias ssh-add -Value "C:\Program Files (x86)\Git\bin\ssh-add.exe"
+  Set-Alias ssh-agent -Value "C:\Program Files (x86)\Git\bin\ssh-agent.exe"
+  Set-Alias ssh-keygen -Value "C:\Program Files (x86)\Git\bin\ssh-keygen.exe"
+  Set-Alias ssh-keyscan -Value "C:\Program Files (x86)\Git\bin\ssh-keyscan.exe"
+  Set-Alias ssh -Value "C:\Program Files (x86)\Git\bin\ssh.exe"
+  Set-Alias tail -Value "C:\Program Files (x86)\Git\bin\tail.exe"
+  Set-Alias tar -Value "C:\Program Files (x86)\Git\bin\tar.exe"
+  Set-Alias tclsh -Value "C:\Program Files (x86)\Git\bin\tclsh.exe"
+  Set-Alias tclsh85 -Value "C:\Program Files (x86)\Git\bin\tclsh85.exe"
+  Set-Alias touch -Value "C:\Program Files (x86)\Git\bin\touch.exe"
+  Set-Alias tr -Value "C:\Program Files (x86)\Git\bin\tr.exe"
+  Set-Alias true -Value "C:\Program Files (x86)\Git\bin\true.exe"
+  Set-Alias uname -Value "C:\Program Files (x86)\Git\bin\uname.exe"
+  Set-Alias uniq -Value "C:\Program Files (x86)\Git\bin\uniq.exe"
+  Set-Alias unix2dos -Value "C:\Program Files (x86)\Git\bin\unix2dos.exe"
+  Set-Alias unzip -Value "C:\Program Files (x86)\Git\bin\unzip.exe"
+  Set-Alias wc -Value "C:\Program Files (x86)\Git\bin\wc.exe"
+  Set-Alias wish -Value "C:\Program Files (x86)\Git\bin\wish.exe"
+  Set-Alias wish85 -Value "C:\Program Files (x86)\Git\bin\wish85.exe"
+  Set-Alias xargs -Value "C:\Program Files (x86)\Git\bin\xargs.exe"
+}
