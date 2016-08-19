1cd45fab14dbc0662380581b51834ea398c64056
Phil <schwartzmx@gmail.com>
2016-08-18 19:14:56 -0500

    ps profile

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,195 +1,141 @@
-Set-Location $HOME
+
+Set-Alias cl Clear-Host
+Set-Alias import Import-Module
+Set-Alias which get-command
+Set-Alias open explorer
+Set-Variable $HOME ~
 Set-Variable HOSTS "C:\Windows\System32\drivers\etc\hosts" -Force
 
-# Load posh-git example profile
-if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
-. 'C:\tools\poshgit\dahlbyk-posh-git-869d4c5\profile.example.ps1'
-Rename-Item Function:\Prompt PoshGitPrompt -Force
-function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
+# Linux-like du 
+Function PoSh-DiskUsage {
+    param(
+        [String]$Path=$($pwd.Path)
+    )
+    Get-ChildItem $path | % {
+        $file = $_; 
+        Get-ChildItem -Recurse $_.FullName | 
+        Measure-Object -Property Length -Sum | 
+        Select  @{ Name= "Name"; Expression= { $file } }, `
+            @{ Name = "Sum (GB)"; Expression = {  "{0:N3}" -f ($_.Sum / 1024MB) } }, `
+            @{ Name = "Sum (MB)"; Expression = {  "{0:N3}" -f ($_.Sum / 1MB) } }, `
+            Sum 
+        } | 
+        Sort Sum -Desc | 
+        Format-Table -Property Name,"Sum (GB)", "Sum (MB)", Sum -AutoSize
+}
+Set-Alias -Name ps-du -Value PoSh-DiskUsage -Description "linux du-like utility"
 
-# Python 3, used when Python 2.7 is also installed
-Set-Alias python3 -Value C:\Python34\python.exe 
-Set-Alias pip3 -Value pip3.4.exe
-
-# Kill all Chrome Helper (tabs) sessions
-Function Chrome-Kill {
-    Get-Process | Where { $_.ProcessName -eq "chrome" } | Select -Skip 1 | ForEach-Object { $_.Kill() }
+##################### GIT Function ###############################
+Function isCurrentDirectoryGitRepository {
+    if ((Test-Path ".git") -eq $TRUE) {
+        return $TRUE
+    }
+    
+    # Test within parent dirs
+    $checkIn = (Get-Item .).parent
+    while ($checkIn -ne $NULL) {
+        $pathToTest = $checkIn.fullname + '/.git'
+        if ((Test-Path $pathToTest) -eq $TRUE) {
+            return $TRUE
+        } else {
+            $checkIn = $checkIn.parent
+        }
+    }
+    
+    return $FALSE
 }
 
-# For Editing your PowerShell profile
-Function Edit-Profile
-{
-    vim $profile
+# Get the current branch
+Function gitBranchName {
+    $currentBranch = ''
+    git branch | foreach {
+        if ($_ -match "^\* (.*)") {
+            $currentBranch += $matches[1]
+        }
+    }
+    return $currentBranch
 }
 
-# Edit hosts file
-Function Edit-Hosts
-{
-  vim $HOSTS
+# Extracts status details about the repo
+Function gitStatus {
+    $untracked = $FALSE
+    $added = 0
+    $modified = 0
+    $deleted = 0
+    $ahead = $FALSE
+    $aheadCount = 0
+    
+    $output = git status
+    
+    $branchbits = $output[0].Split(' ')
+    $branch = $branchbits[$branchbits.length - 1]
+    
+    $output | foreach {
+        if ($_ -match "^\#.*origin/.*' by (\d+) commit.*") {
+            $aheadCount = $matches[1]
+            $ahead = $TRUE
+        }
+        elseif ($_ -match "deleted:") {
+            $deleted += 1
+        }
+        elseif (($_ -match "modified:") -or ($_ -match "renamed:")) {
+            $modified += 1
+        }
+        elseif ($_ -match "new file:") {
+            $added += 1
+        }
+        elseif ($_ -match "Untracked files:") {
+            $untracked = $TRUE
+        }
+    }
+    
+    return @{"untracked" = $untracked;
+             "added" = $added;
+             "modified" = $modified;
+             "deleted" = $deleted;
+             "ahead" = $ahead;
+             "aheadCount" = $aheadCount;
+             "branch" = $branch}
+}
+##################### GIT Function ###############################
+
+
+######################## Prompt ##################################
+Function prompt {
+	$path = $pwd.Path.replace("$HOME", "~")
+    Write-Host $env:username -NoNewline -ForegroundColor Green
+	Write-Host '@' -NoNewline -ForegroundColor Gray
+    Write-Host $([System.Environment]::MachineName)  -NoNewline -ForegroundColor Green
+    Write-Host ' {' -NoNewline -ForegroundColor Gray
+    Write-Host $path -NoNewline -ForegroundColor Yellow
+    Write-Host '} ' -NoNewline -ForegroundColor Gray
+	$host.UI.RawUi.WindowTitle = $userLocation
+    Write-Host($userLocation) -nonewline -foregroundcolor Yellow    
+    if (isCurrentDirectoryGitRepository) {
+        $status = gitStatus
+        $currentBranch = $status["branch"]        
+        Write-Host('[') -nonewline -foregroundcolor Gray
+        if ($status["ahead"] -eq $FALSE) {
+            # We are not ahead of origin
+            Write-Host($currentBranch) -nonewline -foregroundcolor Cyan
+        } else {
+            # We are ahead of origin
+            Write-Host($currentBranch) -nonewline -foregroundcolor Red
+        }
+        Write-Host(' +' + $status["added"]) -nonewline -foregroundcolor Yellow
+        Write-Host(' ~' + $status["modified"]) -nonewline -foregroundcolor Yellow
+        Write-Host(' -' + $status["deleted"]) -nonewline -foregroundcolor Yellow      
+        if ($status["untracked"] -ne $FALSE) {
+            Write-Host(' !') -nonewline -foregroundcolor Yellow
+        }       
+        Write-Host('] ') -nonewline -foregroundcolor Gray 
+    }    
+	Write-Host('$') -nonewline -foregroundcolor Green    
+	return " "
 }
 
-# For Editing your Vim settings
-Function Edit-Vimrc
-{
-    vim $home\_vimrc
-}
-
-# Find files with a specified string in them
-# String Search Directory > ss-dir Directory String
-Function ss-dir {
-	If (Test-Path -Path $args[0] -PathType Container) {
-		Get-ChildItem $args[0] -recurse | Select-String -pattern “$args[1]” | Group path | Select name
-	}
-	Else {
-		Write-Host "Path specified is not a directory. Usage: ss-dir Directory String"
-	}
-}
-
-# Quick access to Projects
 Function p
 {
-    cd C:\Users\Fed-Phil\Projects
-}
-# Remove recursive force
-Function rmrf {
-    for($i=0; $i -lt $args.length; $i++){
-        Remove-Item -Recurse -Force $args[$i]
-    }
-}
-
-
-function New-Zip
-{
-    param([string]$zipfilename)
-    set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
-    (dir $zipfilename).IsReadOnly = $false
-}
-# usage: new-zip c:\demo\myzip.zip
-
-function Add-Zip
-{
-    param([string]$zipfilename)
-
-    if(-not (test-path($zipfilename))) {
-        set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
-        (dir $zipfilename).IsReadOnly = $false  
-    }
-                        
-    $shellApplication = new-object -com shell.application
-    $zipPackage = $shellApplication.NameSpace($zipfilename)
-                              
-    foreach($file in $input) { 
-        $zipPackage.CopyHere($file.FullName)
-        Start-sleep -milliseconds 500
-    }
-}
-#usage: dir c:\demo\files\*.* -Recurse | add-Zip c:\demo\myzip.zip
-
-function Get-Zip
-{
-    param([string]$zipfilename)
-    if(test-path($zipfilename)) {
-        $shellApplication = new-object -com shell.application
-        $zipPackage = $shellApplication.NameSpace($zipfilename)
-        $zipPackage.Items() | Select Path
-    }
-}
-
-#usage: Get-Zip c:\demo\myzip.zip
-
-function Extract-Zip
-{
-    param([string]$zipfilename, [string] $destination)
-
-    if(test-path($zipfilename)) { 
-        $shellApplication = new-object -com shell.application
-        $zipPackage = $shellApplication.NameSpace($zipfilename)
-        $destinationFolder = $shellApplication.NameSpace($destination)
-        $destinationFolder.CopyHere($zipPackage.Items())
-    }
-}
-
-
-New-Alias import Import-Module
-New-Alias cl clear
-New-Alias which get-command
-New-Alias open explorer
-Set-Alias notepad -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
-Set-Alias subl -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
-
-# Assumes GitBash is installed
-# $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"      
-If (Test-Path -Path "C:\Program Files (x86)\Git\bin\" -PathType Container) {
-  Set-Alias antiword -Value "C:\Program Files (x86)\Git\bin\antiword.exe"
-  Set-Alias basename -Value "C:\Program Files (x86)\Git\bin\basename.exe"
-  Set-Alias bash -Value "C:\Program Files (x86)\Git\bin\bash.exe"
-  Set-Alias bison -Value "C:\Program Files (x86)\Git\bin\bison.exe"
-  Set-Alias bzip2 -Value "C:\Program Files (x86)\Git\bin\bzip2.exe"
-  Set-Alias chmod -Value "C:\Program Files (x86)\Git\bin\chmod.exe"
-  Set-Alias cmp -Value "C:\Program Files (x86)\Git\bin\cmp.exe"
-  Set-Alias connect -Value "C:\Program Files (x86)\Git\bin\connect.exe"
-  Set-Alias cut -Value "C:\Program Files (x86)\Git\bin\cut.exe"
-  Set-Alias date -Value "C:\Program Files (x86)\Git\bin\date.exe"
-  Set-Alias dirname -Value "C:\Program Files (x86)\Git\bin\dirname.exe"
-  Set-Alias dos2unix -Value "C:\Program Files (x86)\Git\bin\dos2unix.exe"
-  Set-Alias du -Value "C:\Program Files (x86)\Git\bin\du.exe"
-  Set-Alias env -Value "C:\Program Files (x86)\Git\bin\env.exe"
-  Set-Alias expr -Value "C:\Program Files (x86)\Git\bin\expr.exe"
-  Set-Alias false -Value "C:\Program Files (x86)\Git\bin\false.exe"
-  Set-Alias find -Value "C:\Program Files (x86)\Git\bin\find.exe"
-  Set-Alias flex -Value "C:\Program Files (x86)\Git\bin\flex.exe"
-  Set-Alias gawk -Value "C:\Program Files (x86)\Git\bin\gawk.exe"
-  Set-Alias getcp -Value "C:\Program Files (x86)\Git\bin\getcp.exe"
-  Set-Alias git -Value "C:\Program Files (x86)\Git\bin\git.exe"
-  Set-Alias gpg -Value "C:\Program Files (x86)\Git\bin\gpg.exe"
-  Set-Alias gpgkeys_curl -Value "C:\Program Files (x86)\Git\bin\gpgkeys_curl.exe"
-  Set-Alias gpgkeys_finger -Value "C:\Program Files (x86)\Git\bin\gpgkeys_finger.exe"
-  Set-Alias gpgkeys_hkp -Value "C:\Program Files (x86)\Git\bin\gpgkeys_hkp.exe"
-  Set-Alias gpgkeys_ldap -Value "C:\Program Files (x86)\Git\bin\gpgkeys_ldap.exe"
-  Set-Alias gpgsplit -Value "C:\Program Files (x86)\Git\bin\gpgsplit.exe"
-  Set-Alias gpgv -Value "C:\Program Files (x86)\Git\bin\gpgv.exe"
-  Set-Alias grep -Value "C:\Program Files (x86)\Git\bin\grep.exe"
-  Set-Alias gunzip -Value "C:\Program Files (x86)\Git\bin\gunzip.exe"
-  Set-Alias gzip -Value "C:\Program Files (x86)\Git\bin\gzip.exe"
-  Set-Alias hd2u -Value "C:\Program Files (x86)\Git\bin\hd2u.exe"
-  Set-Alias head -Value "C:\Program Files (x86)\Git\bin\head.exe"
-  Set-Alias iconv -Value "C:\Program Files (x86)\Git\bin\iconv.exe"
-  Set-Alias id -Value "C:\Program Files (x86)\Git\bin\id.exe"
-  Set-Alias less -Value "C:\Program Files (x86)\Git\bin\less.exe"
-  Set-Alias ln -Value "C:\Program Files (x86)\Git\bin\ln.exe"
-  Set-Alias m4 -Value "C:\Program Files (x86)\Git\bin\m4.exe"
-  Set-Alias md5sum -Value "C:\Program Files (x86)\Git\bin\md5sum.exe"
-  Set-Alias mkdir -Value "C:\Program Files (x86)\Git\bin\mkdir.exe"
-  Set-Alias msmtp -Value "C:\Program Files (x86)\Git\bin\msmtp.exe"
-  Set-Alias openssl -Value "C:\Program Files (x86)\Git\bin\openssl.exe"
-  Set-Alias patch -Value "C:\Program Files (x86)\Git\bin\patch.exe"
-  Set-Alias pdfinfo -Value "C:\Program Files (x86)\Git\bin\pdfinfo.exe"
-  Set-Alias pdftotext -Value "C:\Program Files (x86)\Git\bin\pdftotext.exe"
-  Set-Alias perl -Value "C:\Program Files (x86)\Git\bin\perl.exe"
-  Set-Alias rebase -Value "C:\Program Files (x86)\Git\bin\rebase.exe"
-  Set-Alias rxvt -Value "C:\Program Files (x86)\Git\bin\rxvt.exe"
-  Set-Alias scp -Value "C:\Program Files (x86)\Git\bin\scp.exe"
-  Set-Alias sed -Value "C:\Program Files (x86)\Git\bin\sed.exe"
-  Set-Alias sh -Value "C:\Program Files (x86)\Git\bin\sh.exe"
-  Set-Alias split -Value "C:\Program Files (x86)\Git\bin\split.exe"
-  Set-Alias ssh-add -Value "C:\Program Files (x86)\Git\bin\ssh-add.exe"
-  Set-Alias ssh-agent -Value "C:\Program Files (x86)\Git\bin\ssh-agent.exe"
-  Set-Alias ssh-keygen -Value "C:\Program Files (x86)\Git\bin\ssh-keygen.exe"
-  Set-Alias ssh-keyscan -Value "C:\Program Files (x86)\Git\bin\ssh-keyscan.exe"
-  Set-Alias ssh -Value "C:\Program Files (x86)\Git\bin\ssh.exe"
-  Set-Alias tail -Value "C:\Program Files (x86)\Git\bin\tail.exe"
-  Set-Alias tar -Value "C:\Program Files (x86)\Git\bin\tar.exe"
-  Set-Alias tclsh -Value "C:\Program Files (x86)\Git\bin\tclsh.exe"
-  Set-Alias tclsh85 -Value "C:\Program Files (x86)\Git\bin\tclsh85.exe"
-  Set-Alias touch -Value "C:\Program Files (x86)\Git\bin\touch.exe"
-  Set-Alias tr -Value "C:\Program Files (x86)\Git\bin\tr.exe"
-  Set-Alias true -Value "C:\Program Files (x86)\Git\bin\true.exe"
-  Set-Alias uname -Value "C:\Program Files (x86)\Git\bin\uname.exe"
-  Set-Alias uniq -Value "C:\Program Files (x86)\Git\bin\uniq.exe"
-  Set-Alias unix2dos -Value "C:\Program Files (x86)\Git\bin\unix2dos.exe"
-  Set-Alias unzip -Value "C:\Program Files (x86)\Git\bin\unzip.exe"
-  Set-Alias wc -Value "C:\Program Files (x86)\Git\bin\wc.exe"
-  Set-Alias wish -Value "C:\Program Files (x86)\Git\bin\wish.exe"
-  Set-Alias wish85 -Value "C:\Program Files (x86)\Git\bin\wish85.exe"
-  Set-Alias xargs -Value "C:\Program Files (x86)\Git\bin\xargs.exe"
+    If (Test-Path E:\Projects) { cd E:\Projects }
+    Else { Throw "E:\Projects does not exist!" }
 }
